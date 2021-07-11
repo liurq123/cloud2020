4150ca0959d075eb53019346ffd99cf2e52036fd
liurq123 <934905651@qq.com>
2021-07-12 00:15:27 +0800

    手动刷新解决客户端配置没法动态刷新问题

---
+++ cloud-config-client3355/src/main/java/com/dkf/springcloud/Controller/ConfigClientController.java
@@ -0,0 +1,19 @@
+package com.dkf.springcloud.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String info;
+
+    @GetMapping("/configinfo")
+    public String getConfigInfo(){
+        return info;
+    }
+}
+
